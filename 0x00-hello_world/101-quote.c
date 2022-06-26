1e3d8e9cd57a8460419c57e4a156fdf3efbae992
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-06-26 16:15:13 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main:where the program starts
+ * Nothing for here
+ * Return should be one.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" -" Dora Korpar, 2015-10-19\n");
+	return (1);
+}
