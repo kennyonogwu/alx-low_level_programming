0ee47de771be98e7b520c2c060f0a0d160f3f284
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-06-26 13:36:38 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main - This is the entry point
+ *
+ * Return is the last bus stop.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
