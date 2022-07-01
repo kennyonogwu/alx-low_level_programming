5db8114c36620c7b5f07c2fbf72cfb76d49b8936
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-06-30 17:48:57 -0700

    Type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	printf("size of char: %lu byte\n", sizeof(char));
-	printf("size of int: %lu bytes\n", sizeof(int));
-	printf("size of long int: %lu bytes\n", sizeof(long int));
-	printf("size of long long int: %lu bytes\n", sizeof(long long int));
-	printf("size of float: %lu bytes\n", sizeof(float));
+	printf("size of char: %ld byte\n", sizeof(char));
+	printf("size of int: %ld bytes\n", sizeof(int));
+	printf("size of long int: %ld bytes\n", sizeof(long int));
+	printf("size of long long int: %ld bytes\n", sizeof(long long int));
+	printf("size of float: %ld bytes\n", sizeof(float));
 	return (0);
 }
