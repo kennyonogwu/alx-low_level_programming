17ab81bff3a9a98d0517eec18bf70e8be14907e2
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-06-30 17:18:27 -0700

    Types sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-/** Main:The main is where the program starts
- *
+
+/** 
+ * Main:The main is where the program starts
+ *Nothing here
  *The Return is where the declared variable ends
  */
+
 int main(void)
 {
-	char 		charactera;
-	int 		integera;
+	char		charactera;
+	int		integera;
 	float		floata;
 	long		longa;
-	long int	longlonga;
-	long long int 	longlonginta;
-
+	long int	longinta;
+	
 	printf("size of char: %lu byte\n", sizeof(charactera));
 	printf("size of int: %lu bytes\n", sizeof(integera));
 	printf("size of float: %lu bytes\n", sizeof(floata));
 	printf("size of long: %lu bytes\n", sizeof(longa));
-	printf("size of long int: %lu bytes\n", sizeof(longlonga));
-	printf("size of long long int: %lu bytes\n", sizeof(longlonginta));
+	printf("size of long int: %lu bytes\n", sizeof(longinta));
+	
 	return (0);
 }
