344202973209f6c5a8eba79d3a8d58b59f114d98
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-07-01 16:09:38 -0700

    Mandatory

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,6 @@
 
 
 /**
- *
  * main -Print all single digit numbers
  *
  * Return: Always 0 (Success)
@@ -14,17 +13,10 @@ int main(void)
 {
 
 		int digit;
-
-
-
 			for (digit = 0; digit < 10; digit++)
-
-					printf("%i", digit);
-
+				printf("%i", digit);
 				putchar('\n');
 
-
-
-					return (0);
+		return (0);
 
 }
