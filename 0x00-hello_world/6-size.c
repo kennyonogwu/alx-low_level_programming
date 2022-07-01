58ab78bd264ad83491b1ab702d72e3c547019ed3
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-06-30 17:40:34 -0700

    Type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-/** 
+/**
  *main -The main is where the program starts
- * 
+ *
  *Return:is where the declared variable ends
  */
 
 int main(void)
-{	
+{
 	printf("size of char: %lu byte\n", sizeof(char));
 	printf("size of int: %lu bytes\n", sizeof(int));
 	printf("size of float: %lu bytes\n", sizeof(float));
 	printf("size of long int: %lu bytes\n", sizeof(long int));
 	printf("size of long long int: %lu bytes\n", sizeof(long long int));
-	
+
 	return (0);
 }
