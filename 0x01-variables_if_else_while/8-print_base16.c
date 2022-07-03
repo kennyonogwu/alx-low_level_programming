2cf12e29b6962a7af0d06c9a8a816a3eb67b42ba
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-07-03 16:01:26 -0700

    Base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,30 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
+	char digit;
 
-			char digit;
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+	for (digit = 'a'; digit <= 'f'; digit++)
+		putchar(digit);
+		putchar('\n');
 
-
-
-					for (digit = '0'; digit <= '9'; digit++)
-
-								putchar(digit);
-
-
-
-							for (digit = 'a'; digit <= 'f'; digit++)
-
-										putchar(digit);
-
-
-
-									putchar('\n');
-
-
-					return (0);
-
+	return (0);
 }
