055a76b3974bc285b336e74e20db57a9ec89d752
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-07-03 17:46:56 -0700

    Mandatory task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,5 @@ int main(void)
 	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("size of a float: %ld byte(s)\n", sizeof(float));
-
 	return (0);
 }
