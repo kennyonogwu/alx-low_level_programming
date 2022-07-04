5eaa63645f6c8543e9590e03e8bb1cc3d3b08075
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-07-03 17:06:33 -0700

    Mandatory tasks

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 /**
- *
  * main - Print all possible combinations of single digit numbers
  *
  * Return: Always 0 (Success)
- *
  */
-
 int main(void)
-
 {
+	int number;
 
-			int number;
-
-
-
-			for (number = 0; number <= 9; number++)
-								{
-				putchar(number + '0');
-
-			if (number < 9)
+	for (number = 0; number <= 9; number++)
 {
-				putchar(',');																			putchar(' ');
-
-																				}
-
+		putchar(number + '0');
+	if (number < 9)
+{	
+	putchar(',');
+	putchar(' ');
 }
-			putchar('\n');
-			return (0);
-
+}
+	putchar('\n');
+return (0);
 }
