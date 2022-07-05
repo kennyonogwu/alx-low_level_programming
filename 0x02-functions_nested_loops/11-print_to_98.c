a47d8d051a501fb4730d5e9ef3353ef6c3fc428b
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-07-05 15:23:27 -0700

    Difficult

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,23 +11,122 @@
  */
 
 
+void case_1(int);
+void case_2(int);
+void case_3(int);
 
 void print_to_98(int n)
+{
+int k = n;
+		case_1(k);
+		case_2(k);
+		case_3(k);
+}
+/**
+ *case_1- prints up to 98 when n is less than 98.
+ *@n:argument passed-printing starts from n.
+ *Return:void.
+ *
+ */
+void case_1(int n)
+{
+	int i;
+	
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+			{
+				printf("%d", i);
+				if (i == 98)
+				{
+					break;
+				}
+				
+					else
+						{
+							printf(",");
+							printf(" ");
+						}
+			}
+		printf("\n");
+	}
+}
+/**
+ *
+ *  *case_2- prints up to 98 when n is greater than 98.
+ *
+ *   *@n:Argument passed, printing starts here.
+ *
+ *    *Return-void.
+ *
+ *     */
+
+void case_2(int n)
 
 {
 
-		if (n <= 98)
+		int i;
 
-					for (n = n; n <= 97; n++)
 
-									printf("%d, ", n);
 
-			else
+			if (n > 98)
 
-						for (n = n; n > 98; n--)
+					{
 
-										printf("%d, ", n);
+								for (i = n; i >= 98; i--)
 
-				printf("98\n");
+											{
+
+															printf("%d", i);
+
+																		if (i == 98)
+
+																						{
+
+																											break;
+
+																														}
+
+																					else
+
+																									{
+
+																														printf(",");
+
+																																		printf(" ");
+
+																																					}
+
+																							}
+
+										printf("\n");
+
+											}
+
+}
+
+/**
+ *
+ *  *case_3-prints  98.
+ *
+ *   *@n:argument passed must be 98.
+ *
+ *    *Return 0.
+ *
+ *     */
+
+void case_3(int n)
+
+{
+
+		if (n == 98)
+
+				{
+
+							printf("%d", n);
+
+									printf("\n");
+
+										}
 
 }
