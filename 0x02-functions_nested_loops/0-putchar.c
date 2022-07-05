c16621b4c464f97f8566ef1d2de9c797b43c2254
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-07-05 10:55:57 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -The start og the program
+ *
+ * Return: Always zero 0
+ */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
