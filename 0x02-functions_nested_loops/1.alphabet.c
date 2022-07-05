1a1c8967ae396664887d175b51a29c85ab964e54
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-07-05 11:40:09 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1.alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main -the start of the program
+ *
+ * Return: Always zero 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i=97; i<123; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+
+}
