40f74a27a47ae99f43936b90ca9784f288b9bdda
kennyonogwu <99299864+kennyonogwu@users.noreply.github.com>
2022-07-08 08:16:26 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,38 +1,35 @@
-#include <stdio.h>
-#define stopVar 100
+#include "main.h"
 
 /**
- * main - main function
- * checks through numbers from 1 - 100
- * and print number if it is not a multiple of 3 or 5
- * print fizz if its a multiple of 3
- * buzz if its a multiple of 5
- * and fizz buzz if its both a multiple of 3 and 5
+ * print_most_numbers - print numbers from 0-9 excluding 2 and 4
  *
- * Created by: Sanctus-Peter
+ * Created by: sanctus-Peter
  * cc: 7th july, 2022
- *
- * Return: 0 if successful
  */
 
-int main(void)
+void print_most_numbers(void)
 {
-	int ii = 1;
+	int var = 0;
 
 	do {
-		if ((ii % 3 == 0) && (ii % 5 == 0))
-			printf("FizzBuzz");
-		else if (ii % 3 == 0)
-			printf("Fizz");
-		else if (ii % 5 == 0)
-			printf("Buzz");
+		if (var == 4 || var == 2)
+		{
+			var++;
+			continue;
+		}
 		else
-			printf("%d", ii);
-		if (ii != stopVar)
-			printf(" ");
-		ii++;
-	} while (ii < stopVar + 1);
-	printf("\n");
+			_putchar(var + '0');
+		var++;
+	} while (var < 10);
 
-	return (0);
+	_putchar('\n');
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
