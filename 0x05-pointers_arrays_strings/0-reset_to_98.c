5937eaf80f177305bc3918365ccc85bafe7b3404
kennyonogwu <kennethnnaemeka83@gmail.com>
2022-07-12 15:25:59 -0700

    Mandatory task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * Write a function that takes a pointer to an int as parameter and updates the value it points to to 98
+ *
+ * Return is always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
