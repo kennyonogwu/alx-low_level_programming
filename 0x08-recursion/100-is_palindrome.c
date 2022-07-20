0668befe24d20693ec4eb19568ead718a822bb50
kennyonogwu <99299864+kennyonogwu@users.noreply.github.com>
2022-07-21 00:50:27 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,45 +1,56 @@
 #include "main.h"
 
-/**
- * _strlen_recursion - computes the length of a string
- * @n: input string
- * Return: length of n
- */
-
-int _strlen_recursion(char *n)
-	{
-		if (!*n)
-		{
-			return (0);
-		}
-		return (1 + _strlen_recursion(++n));
-	}
+int _strlen(char *s);
+int check_palindrome(char *s, int start, int end);
 
 /**
- * ispal- helper function for is_palindrome
- * @str: input string
- * @s: start index
- * @e: end index
- * Return: 1 if true, 0 if false
- */
-int ispal(char str[], int s, int e)
-	{
-		if (s == e)
-			return (1);
-		if (str[s] != str[e])
-			return (0);
-		if (s < e + 1)
-			return (ispal(str, s + 1, e - 1));
-		return (1);
-	}
-
-/**
- * is_palindrome - returns 1 if a string is a palindrome and 0 if not
- * @s: input string
- * Return: 1 if true, 0 if false
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s: the string
+ *
+ * Return: returns 1 if a string is a palindrome and 0 if not.
  */
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s) - 1;
-	return (ispal(s, 0, len));
+	int len = _strlen(s);
+
+	if (len == 0)
+		return (1);
+
+	return (check_palindrome(s, 0, len - 1));
+}
+
+/**
+ * _strlen - gets the length of a string
+ * @s: the string
+ *
+ * Return: the string length
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (_strlen(++s) + 1);
+}
+
+/**
+ * check_palindrome - checks if a substring is a palindrome
+ * @s: the string
+ * @start: the starting index of substring
+ * @end: the stopping index of substring
+ *
+ * Return: 1 if substring is a palindrome, otherwise 0
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start == end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	if (start < end + 1)
+		return (check_palindrome(s, start + 1, end - 1));
+
+	return (1);
 }
